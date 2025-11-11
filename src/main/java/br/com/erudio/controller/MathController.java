44b0f8b0c4d110cf952b7f32b615f97ae1c97db1
Carlos Gonzaga <mso.devcode@gmail.com>
2025-11-11 15:57:33 -0300

    feat: Done the all the math operations and converters method

--- src/main/java/br/com/erudio/controller/MathController.java
+++ src/main/java/br/com/erudio/controller/MathController.java
@@ -11,8 +11,75 @@ public class MathController {
 
     @RequestMapping("/sum/{number01}/{number02}")
     public Double sum(@PathVariable String number01,
-                      @PathVariable String number02) {
-        return 1d;
-
+                      @PathVariable String number02)  throws IllegalArgumentException {
+            if (!isNumeric(number01) || !isNumeric(number02))  {
+                throw new IllegalArgumentException("This is not a number");
+            }
+            return convertToDouble(number01) + convertToDouble(number02);
     }
+
+    @RequestMapping("/subtraction/{number01}/{number02}")
+    public Double subtraction(@PathVariable String number01,
+                      @PathVariable String number02)  throws IllegalArgumentException {
+        if (!isNumeric(number01) || !isNumeric(number02))  {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        return convertToDouble(number01) - convertToDouble(number02);
+    }
+
+    @RequestMapping("/multiplication/{number01}/{number02}")
+    public Double multiplication(@PathVariable String number01,
+                      @PathVariable String number02)  throws IllegalArgumentException {
+        if (!isNumeric(number01) || !isNumeric(number02))  {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        return convertToDouble(number01) * convertToDouble(number02);
+    }
+
+    @RequestMapping("/division/{number01}/{number02}")
+    public Double division(@PathVariable String number01,
+                                 @PathVariable String number02)  throws IllegalArgumentException {
+        if (!isNumeric(number01) || !isNumeric(number02))  {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        return convertToDouble(number01) / convertToDouble(number02);
+    }
+
+    @RequestMapping("/average/{number01}/{number02}")
+    public Double average(@PathVariable String number01,
+                          @PathVariable String number02)  throws IllegalArgumentException {
+        if (!isNumeric(number01) || !isNumeric(number02))  {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        return (convertToDouble(number01) + convertToDouble(number02))/ 2;
+    }
+
+    @RequestMapping("/rootSquare/{number}")
+    public Double rootSquare(@PathVariable String number) throws IllegalArgumentException {
+        if (!isNumeric(number))  {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        return Math.sqrt(Double.parseDouble(number));
+    }
+
+
+
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException{
+        if (strNumber == null || strNumber.isEmpty()) {
+            throw new IllegalArgumentException("This is not a number");
+        }
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    private Boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) {
+            return false;
+        }
+        String number = strNumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+
 }
